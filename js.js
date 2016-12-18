7a2e5c1e061642d87a1b98a3bbe35150cb492f58
sjwinther <sebastianwinther@gmail.com>
2016-12-18 20:19:44 +0100

    Added local fonts and query

--- js.js
+++ js.js
@@ -1,14 +1,13 @@
 $(document).ready(function() {
 
   //Clicking on the mobile-menu toggles a list of menu-items
-  //On the frontpage, it also toggles some extra padding, so the menu and the content do no overlap
   $("#burger").click(function() {
     $(this).toggleClass("line-through");
     $("#menu").slideToggle(200);
     return false;
   });
 
-  //Upon window resize, the burger-style menu closes and the menu items reappears normally
+  //Upon window resize, the burger-style menu closes. If the window reaches a width above 500px the menu items reappear as normal
   $(window).resize(function() {
     var $width = $(window).width();
     if($width >= 500) {
